1f52554cf83aa04edda6b289b54d1a56b0742c50
Minho Lee <minho.lee.93@hotmail.com>
2017-09-14 16:27:08 -0400

    make manifest, attempting to add in STT

--- app.js
+++ app.js
@@ -4,8 +4,48 @@ let fs = require('fs');
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
-var cfenv = require('cfenv');
+var cfenv = require('cfenv'),
+		watson = require('watson-developer-cloud'),
+		bluemix = require('./config/bluemix'),
+		extend = require('util')._extend;
 
+// For local development, put username and password in config
+// or store in your environment
+var config = {
+    version: 'v1',
+    url: 'https://stream.watsonplatform.net/speech-to-text/api',
+    username: "676a5bd9-8cd8-4d79-8a6c-8790137934d6",
+    password: "P0Lz3XYep07Q"
+};
+
+// redirect to https if the app is not running locally
+if (!!process.env.VCAP_SERVICES) {
+    app.enable('trust proxy');
+    app.use(function(req, res, next) {
+        if (req.secure) {
+            next();
+        } else {
+            res.redirect('https://' + req.headers.host + req.url);
+        }
+    });
+}
+
+// if bluemix credentials exists, then override local
+var credentials = extend(config, bluemix.getServiceCreds('speech_to_text'));
+var authorization = watson.authorization(credentials);
+
+// Get token from Watson using your credentials
+app.get('/token', function(req, res) {
+    authorization.getToken({
+        url: credentials.url
+    }, function(err, token) {
+        if (err) {
+            console.log('error:', err);
+            res.status(err.code);
+        }
+        res.send(token);
+    });
+});
 
 app.use(bodyParser.urlencoded({
    extended: false
@@ -28,7 +68,7 @@ app.use(function(req,res,next) {
 		}
 	};
 	next();
-})
+});
 
 app.get('/', function(req, res) {
   res.render('main');
