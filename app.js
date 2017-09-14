400a44745ee6fc0456ddf041abaa02c41866d1a2
Minho Lee <minho.lee.93@hotmail.com>
2017-09-14 14:30:00 -0400

    adding global vars, making changes

--- app.js
+++ app.js
@@ -8,25 +8,39 @@ var cfenv = require('cfenv');
 
 
 app.use(bodyParser.urlencoded({
-    extended: false
+   extended: false
 }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
+
+app.use(function(req,res,next) {
+	res.locals = {
+		site: {
+			title: 'Tone Analyzer App',
+			description: 'Analyzes your tone using speech to text!',
+		},
+		author: {
+			name: 'Minho Lee',
+		}
+	};
+	next();
+})
 
 app.get('/', function(req, res) {
-    res.render('main.ejs');
+  res.render('main');
 });
 app.get('/app', function(req, res) {
-    res.render('app.ejs');
+  res.render('app');
 });
+
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-    // print a message when the server starts listening
-    console.log("server starting on " + appEnv.url);
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
 });
